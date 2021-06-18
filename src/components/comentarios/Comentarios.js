413d8572ca1f4e5d619f1746f11d67fff51a7e17
Dominguez <dominguezdenia7@gmail.com>
2021-06-18 01:39:42 -0600

    modificaciones 17/21

--- src/components/comentarios/Comentarios.js
+++ src/components/comentarios/Comentarios.js
@@ -7,7 +7,7 @@ import { useForm } from '../../hooks/useForm';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, startComment } from '../../actions/auth';
 import Swal from 'sweetalert2';
-import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
 
 
 
@@ -132,7 +132,7 @@ export const Comentarios = () => {
                 showConfirmButton: false,
                 timer: 1500
               });  
-              window.location =<Link to="/login"/>
+              window.location = <Link to="/login"/>
         } 
 
     }
